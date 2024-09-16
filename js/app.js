5cac0db0cae393471ca4bb00ea716742ebc0e07b
Alec74 <alecmcglone@Alecs-Mac-mini.local>
2024-09-16 19:08:06 -0400

    adding content to workout page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('I am running!');
+const homebtn = document.querySelector('.menu-header-content-home');
+const notifbtn = document.querySelector('.menu-header-content-notif');
+
+homebtn.addEventListener('click', function () {
+    location.href = "#"
+}) 
+
+notifbtn.addEventListener('click', function () {
+    console.log('notifications')
+}) 
